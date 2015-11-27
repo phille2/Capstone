cf2cacafb3c3af3b44310dab841435d2140d3ce2
fantaorange <sa_ashfaque@hotmail.com>
2015-11-27 01:33:16 -0500

    Update turningLights_trial.ino

--- Arduino/turningLights_trial/turningLights_trial.ino
+++ Arduino/turningLights_trial/turningLights_trial.ino
@@ -7,22 +7,36 @@ void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   // make the pushbutton's pin an input:
-  pinMode(pushButton, INPUT);
+  pinMode(pushButtonLeft, INPUT);
+  //pinMode (pushButtonRight, INPUT);
   pinMode(13, OUTPUT);
+  //pinMode(12, OUTPUT);
+  
 }
 
 // the loop routine:
 void loop() {
   // read the input pin:
-  int buttonState = digitalRead(pushButton);
+  int buttonStateLeft = digitalRead(pushButtonLeft);
+//  int buttonStateRight = digitalRead(pushButtonRight);
   // print out the state of the button:
-  Serial.println(buttonState);
-
-  if (buttonState != 0){
+  Serial.println(buttonStateLeft);
+  //Serial.println(buttonStateRight); //check if we put in two inputs into one Serial Port
+  if (buttonStateLeft != 0){
       digitalWrite(13,HIGH);
       delay(1000);
       digitalWrite(13,LOW);
       delay(1000);
+    }else {
+      digitalWrite(13,LOW);
+    }
+  if (buttonStateRight != 0){
+      digitalWrite(12,HIGH);
+      delay(1000);
+      digitalWrite(12,LOW);
+      delay(1000);
+    }else {
+      digitalWrite(12,LOW);
     }
   delay(1);        // delay in between reads for stability
 }
