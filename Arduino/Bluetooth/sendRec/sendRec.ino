64eadaaff68f89547713a86f8319c99caa758729
phille2 <eric.phillipsheldon@gmail.com>
2016-01-14 15:13:19 -0500

    Bluetooth Sketches
    First round of bluetooth sketches. Includes two reference sketches used
    for building of Send/Receive sketch.

---
+++ Arduino/Bluetooth/sendRec/sendRec.ino
@@ -0,0 +1,42 @@
+//Eric Phillips-Sheldon
+//January 2016
+//Bluetooth send and receive code
+
+int counter = 0;
+int LED = 13;
+char INBYTE;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(50);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  bool request;
+  request = msgReq();
+  msgSend(request);
+}
+bool msgReq() {
+  Serial.println("Press 1 to request Arduino data");
+  while(!Serial.available());
+  INBYTE = Serial.read();
+  if (INBYTE == '1') {
+    digitalWrite(LED, LOW);
+    delay(50);
+    digitalWrite(LED, HIGH);
+    return true;
+  }
+}
+void msgSend(bool request) {
+  if (request) {
+  counter++;
+  Serial.print("This is message ");
+  Serial.println(counter);
+  delay(500);
+  }
+  request=false; //reset request to false
+}
+
